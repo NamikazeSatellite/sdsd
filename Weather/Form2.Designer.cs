df6af06c6b16ae482d93800c0ad296a64201ad8a
NamikazeSatellite <119170949+NamikazeSatellite@users.noreply.github.com>
2023-06-01 07:57:33 +0400

    Add files via upload

---
+++ Weather/Form2.Designer.cs
@@ -0,0 +1,385 @@
+﻿namespace Weather
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            maxTempLabel2 = new Label();
+            label2 = new Label();
+            pictureBox2 = new PictureBox();
+            warningLabel = new Label();
+            minTempLabel2 = new Label();
+            minTempLabel3 = new Label();
+            pictureBox3 = new PictureBox();
+            maxTempLabel3 = new Label();
+            minTempLabel5 = new Label();
+            pictureBox5 = new PictureBox();
+            maxTempLabel5 = new Label();
+            minTempLabel6 = new Label();
+            pictureBox6 = new PictureBox();
+            maxTempLabel6 = new Label();
+            minTempLabel7 = new Label();
+            pictureBox7 = new PictureBox();
+            maxTempLabel7 = new Label();
+            minTempLabel4 = new Label();
+            pictureBox4 = new PictureBox();
+            maxTempLabel4 = new Label();
+            minTempLabel1 = new Label();
+            pictureBox1 = new PictureBox();
+            maxTempLabel1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // maxTempLabel2
+            // 
+            maxTempLabel2.AutoSize = true;
+            maxTempLabel2.BackColor = Color.Transparent;
+            maxTempLabel2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            maxTempLabel2.ForeColor = Color.White;
+            maxTempLabel2.Location = new Point(118, 318);
+            maxTempLabel2.Name = "maxTempLabel2";
+            maxTempLabel2.Size = new Size(28, 21);
+            maxTempLabel2.TabIndex = 0;
+            maxTempLabel2.Text = "31";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(297, 40);
+            label2.Name = "label2";
+            label2.Size = new Size(190, 37);
+            label2.TabIndex = 1;
+            label2.Text = "5 Day Forecast";
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(135, 259);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(56, 56);
+            pictureBox2.TabIndex = 4;
+            pictureBox2.TabStop = false;
+            // 
+            // warningLabel
+            // 
+            warningLabel.AutoSize = true;
+            warningLabel.BackColor = Color.Transparent;
+            warningLabel.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            warningLabel.ForeColor = Color.White;
+            warningLabel.Location = new Point(300, 421);
+            warningLabel.Name = "warningLabel";
+            warningLabel.Size = new Size(488, 20);
+            warningLabel.TabIndex = 9;
+            warningLabel.Text = "*Forecast data is only available for Tbilisi due to Geocoder API Problems";
+            // 
+            // minTempLabel2
+            // 
+            minTempLabel2.AutoSize = true;
+            minTempLabel2.BackColor = Color.Transparent;
+            minTempLabel2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            minTempLabel2.ForeColor = Color.White;
+            minTempLabel2.Location = new Point(175, 318);
+            minTempLabel2.Name = "minTempLabel2";
+            minTempLabel2.Size = new Size(28, 21);
+            minTempLabel2.TabIndex = 10;
+            minTempLabel2.Text = "31";
+            // 
+            // minTempLabel3
+            // 
+            minTempLabel3.AutoSize = true;
+            minTempLabel3.BackColor = Color.Transparent;
+            minTempLabel3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            minTempLabel3.ForeColor = Color.White;
+            minTempLabel3.Location = new Point(289, 318);
+            minTempLabel3.Name = "minTempLabel3";
+            minTempLabel3.Size = new Size(28, 21);
+            minTempLabel3.TabIndex = 13;
+            minTempLabel3.Text = "31";
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Location = new Point(249, 259);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(56, 56);
+            pictureBox3.TabIndex = 12;
+            pictureBox3.TabStop = false;
+            // 
+            // maxTempLabel3
+            // 
+            maxTempLabel3.AutoSize = true;
+            maxTempLabel3.BackColor = Color.Transparent;
+            maxTempLabel3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            maxTempLabel3.ForeColor = Color.White;
+            maxTempLabel3.Location = new Point(232, 318);
+            maxTempLabel3.Name = "maxTempLabel3";
+            maxTempLabel3.Size = new Size(28, 21);
+            maxTempLabel3.TabIndex = 11;
+            maxTempLabel3.Text = "31";
+            // 
+            // minTempLabel5
+            // 
+            minTempLabel5.AutoSize = true;
+            minTempLabel5.BackColor = Color.Transparent;
+            minTempLabel5.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            minTempLabel5.ForeColor = Color.White;
+            minTempLabel5.Location = new Point(522, 318);
+            minTempLabel5.Name = "minTempLabel5";
+            minTempLabel5.Size = new Size(28, 21);
+            minTempLabel5.TabIndex = 19;
+            minTempLabel5.Text = "31";
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Location = new Point(482, 259);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(56, 56);
+            pictureBox5.TabIndex = 18;
+            pictureBox5.TabStop = false;
+            // 
+            // maxTempLabel5
+            // 
+            maxTempLabel5.AutoSize = true;
+            maxTempLabel5.BackColor = Color.Transparent;
+            maxTempLabel5.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            maxTempLabel5.ForeColor = Color.White;
+            maxTempLabel5.Location = new Point(465, 318);
+            maxTempLabel5.Name = "maxTempLabel5";
+            maxTempLabel5.Size = new Size(28, 21);
+            maxTempLabel5.TabIndex = 17;
+            maxTempLabel5.Text = "31";
+            // 
+            // minTempLabel6
+            // 
+            minTempLabel6.AutoSize = true;
+            minTempLabel6.BackColor = Color.Transparent;
+            minTempLabel6.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            minTempLabel6.ForeColor = Color.White;
+            minTempLabel6.Location = new Point(643, 318);
+            minTempLabel6.Name = "minTempLabel6";
+            minTempLabel6.Size = new Size(28, 21);
+            minTempLabel6.TabIndex = 22;
+            minTempLabel6.Text = "31";
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.Location = new Point(603, 259);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(56, 56);
+            pictureBox6.TabIndex = 21;
+            pictureBox6.TabStop = false;
+            // 
+            // maxTempLabel6
+            // 
+            maxTempLabel6.AutoSize = true;
+            maxTempLabel6.BackColor = Color.Transparent;
+            maxTempLabel6.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            maxTempLabel6.ForeColor = Color.White;
+            maxTempLabel6.Location = new Point(586, 318);
+            maxTempLabel6.Name = "maxTempLabel6";
+            maxTempLabel6.Size = new Size(28, 21);
+            maxTempLabel6.TabIndex = 20;
+            maxTempLabel6.Text = "31";
+            // 
+            // minTempLabel7
+            // 
+            minTempLabel7.AutoSize = true;
+            minTempLabel7.BackColor = Color.Transparent;
+            minTempLabel7.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            minTempLabel7.ForeColor = Color.White;
+            minTempLabel7.Location = new Point(761, 318);
+            minTempLabel7.Name = "minTempLabel7";
+            minTempLabel7.Size = new Size(28, 21);
+            minTempLabel7.TabIndex = 25;
+            minTempLabel7.Text = "31";
+            // 
+            // pictureBox7
+            // 
+            pictureBox7.Location = new Point(721, 259);
+            pictureBox7.Name = "pictureBox7";
+            pictureBox7.Size = new Size(56, 56);
+            pictureBox7.TabIndex = 24;
+            pictureBox7.TabStop = false;
+            // 
+            // maxTempLabel7
+            // 
+            maxTempLabel7.AutoSize = true;
+            maxTempLabel7.BackColor = Color.Transparent;
+            maxTempLabel7.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            maxTempLabel7.ForeColor = Color.White;
+            maxTempLabel7.Location = new Point(704, 318);
+            maxTempLabel7.Name = "maxTempLabel7";
+            maxTempLabel7.Size = new Size(28, 21);
+            maxTempLabel7.TabIndex = 23;
+            maxTempLabel7.Text = "31";
+            // 
+            // minTempLabel4
+            // 
+            minTempLabel4.AutoSize = true;
+            minTempLabel4.BackColor = Color.Transparent;
+            minTempLabel4.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            minTempLabel4.ForeColor = Color.White;
+            minTempLabel4.Location = new Point(405, 318);
+            minTempLabel4.Name = "minTempLabel4";
+            minTempLabel4.Size = new Size(28, 21);
+            minTempLabel4.TabIndex = 28;
+            minTempLabel4.Text = "31";
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Location = new Point(365, 259);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(56, 56);
+            pictureBox4.TabIndex = 27;
+            pictureBox4.TabStop = false;
+            // 
+            // maxTempLabel4
+            // 
+            maxTempLabel4.AutoSize = true;
+            maxTempLabel4.BackColor = Color.Transparent;
+            maxTempLabel4.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            maxTempLabel4.ForeColor = Color.White;
+            maxTempLabel4.Location = new Point(348, 318);
+            maxTempLabel4.Name = "maxTempLabel4";
+            maxTempLabel4.Size = new Size(28, 21);
+            maxTempLabel4.TabIndex = 26;
+            maxTempLabel4.Text = "31";
+            // 
+            // minTempLabel1
+            // 
+            minTempLabel1.AutoSize = true;
+            minTempLabel1.BackColor = Color.Transparent;
+            minTempLabel1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            minTempLabel1.ForeColor = Color.White;
+            minTempLabel1.Location = new Point(63, 318);
+            minTempLabel1.Name = "minTempLabel1";
+            minTempLabel1.Size = new Size(28, 21);
+            minTempLabel1.TabIndex = 31;
+            minTempLabel1.Text = "31";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.AccessibleRole = AccessibleRole.SplitButton;
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox1.Location = new Point(23, 259);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(56, 56);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 30;
+            pictureBox1.TabStop = false;
+            // 
+            // maxTempLabel1
+            // 
+            maxTempLabel1.AutoSize = true;
+            maxTempLabel1.BackColor = Color.Transparent;
+            maxTempLabel1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            maxTempLabel1.ForeColor = Color.White;
+            maxTempLabel1.Location = new Point(6, 318);
+            maxTempLabel1.Name = "maxTempLabel1";
+            maxTempLabel1.Size = new Size(28, 21);
+            maxTempLabel1.TabIndex = 29;
+            maxTempLabel1.Text = "31";
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(800, 450);
+            Controls.Add(minTempLabel1);
+            Controls.Add(pictureBox1);
+            Controls.Add(maxTempLabel1);
+            Controls.Add(minTempLabel4);
+            Controls.Add(pictureBox4);
+            Controls.Add(maxTempLabel4);
+            Controls.Add(minTempLabel7);
+            Controls.Add(pictureBox7);
+            Controls.Add(maxTempLabel7);
+            Controls.Add(minTempLabel6);
+            Controls.Add(pictureBox6);
+            Controls.Add(maxTempLabel6);
+            Controls.Add(minTempLabel5);
+            Controls.Add(pictureBox5);
+            Controls.Add(maxTempLabel5);
+            Controls.Add(minTempLabel3);
+            Controls.Add(pictureBox3);
+            Controls.Add(maxTempLabel3);
+            Controls.Add(minTempLabel2);
+            Controls.Add(warningLabel);
+            Controls.Add(pictureBox2);
+            Controls.Add(label2);
+            Controls.Add(maxTempLabel2);
+            Name = "Form2";
+            Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label maxTempLabel2;
+        private Label label2;
+        private PictureBox pictureBox2;
+        private Label warningLabel;
+        private Label minTempLabel2;
+        private Label minTempLabel3;
+        private PictureBox pictureBox3;
+        private Label maxTempLabel3;
+        private Label minTempLabel5;
+        private PictureBox pictureBox5;
+        private Label maxTempLabel5;
+        private Label minTempLabel6;
+        private PictureBox pictureBox6;
+        private Label maxTempLabel6;
+        private Label minTempLabel7;
+        private PictureBox pictureBox7;
+        private Label maxTempLabel7;
+        private Label minTempLabel4;
+        private PictureBox pictureBox4;
+        private Label maxTempLabel4;
+        private Label minTempLabel1;
+        private PictureBox pictureBox1;
+        private Label maxTempLabel1;
+    }
+}
---
+++ Weather/Form2.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Forecast;
+using Newtonsoft.Json;
+
+namespace Weather;
+
+public partial class Form2 : Form
+{
+    public Form2(string s)
+    {
+        InitializeComponent();
+        GetForecast();
+    }
+
+    void GetForecast()
+    {
+        using (WebClient web = new WebClient())
+        {
+            string url = string.Format("https://api.open-meteo.com/v1/forecast?latitude=41.69&longitude=44.80&daily=weathercode,temperature_2m_max,temperature_2m_min&timezone=Europe%2FMoscow");
+            var json = web.DownloadString(url);
+            ForecastInfo.root info = JsonConvert.DeserializeObject<ForecastInfo.root>(json);
+            maxTempLabel1.Text = Math.Round(info.daily.temperature_2m_max[0]).ToString() + "°";
+            minTempLabel1.Text = Math.Round(info.daily.temperature_2m_min[0]).ToString() + "°";
+            maxTempLabel2.Text = Math.Round(info.daily.temperature_2m_max[1]).ToString() + "°";
+            minTempLabel2.Text = Math.Round(info.daily.temperature_2m_min[1]).ToString() + "°";
+            maxTempLabel3.Text = Math.Round(info.daily.temperature_2m_max[2]).ToString() + "°";
+            minTempLabel3.Text = Math.Round(info.daily.temperature_2m_min[2]).ToString() + "°";
+            maxTempLabel4.Text = Math.Round(info.daily.temperature_2m_max[3]).ToString() + "°";
+            minTempLabel4.Text = Math.Round(info.daily.temperature_2m_min[3]).ToString() + "°";
+            maxTempLabel5.Text = Math.Round(info.daily.temperature_2m_max[4]).ToString() + "°";
+            minTempLabel5.Text = Math.Round(info.daily.temperature_2m_min[4]).ToString() + "°";
+            maxTempLabel6.Text = Math.Round(info.daily.temperature_2m_max[5]).ToString() + "°";
+            minTempLabel6.Text = Math.Round(info.daily.temperature_2m_min[5]).ToString() + "°";
+            maxTempLabel7.Text = Math.Round(info.daily.temperature_2m_max[6]).ToString() + "°";
+            minTempLabel7.Text = Math.Round(info.daily.temperature_2m_min[6]).ToString() + "°";
+            pictureBox1.ImageLocation = GetPath(pictureBox1);
+            pictureBox2.ImageLocation = GetPath(pictureBox2);
+            pictureBox3.ImageLocation = GetPath(pictureBox3);
+            pictureBox4.ImageLocation = GetPath(pictureBox4);
+            pictureBox5.ImageLocation = GetPath(pictureBox5);
+            pictureBox6.ImageLocation = GetPath(pictureBox6);
+            pictureBox7.ImageLocation = GetPath(pictureBox7);
+
+            string GetPath(PictureBox s)
+            {
+                foreach (var item in info.daily.weathercode)
+                {
+                    if (item == 0)
+                        return "C:\\Users\\hp\\Source\\Repos\\Weather\\Data\\01d.png";
+                    else if (item == 1 | item == 2 | item == 3)
+                        return "C:\\Users\\hp\\Source\\Repos\\Weather\\Data\\02.png";
+                    else if (item == 45 | item == 48)
+                        return "C:\\Users\\hp\\Source\\Repos\\Weather\\Data\\50d.png";
+                    else if (item == 51 | item == 53 | item == 55 | item == 56 | item == 57)
+                        return "C:\\Users\\hp\\Source\\Repos\\Weather\\Data\\01d.png";
+                    else if (item == 61 | item == 63 | item == 65 | item == 66 | item == 67)
+                        return "C:\\Users\\hp\\Source\\Repos\\Weather\\Data\\09d.png";
+                    else if (item == 71 | item == 73 | item == 75 | item == 77 | item == 85 | item == 86)
+                        return "C:\\Users\\hp\\Source\\Repos\\Weather\\Data\\13d.png";
+                    else if (item == 80 | item == 81 | item == 82)
+                        return "C:\\Users\\hp\\Source\\Repos\\Weather\\Data\\09d.png";
+                }
+                return "C:\\Users\\hp\\Source\\Repos\\Weather\\Data\\sddddddd.png";
+            }
+            //for (int i = 0; i <= 7; i++)
+            //{
+            //    minTempLabel[i].Text = Math.Round(info.daily.temperature_2m_min[i - 0]).ToString();
+            //}
+
+        }
+
+
+    }
+
+
+}
